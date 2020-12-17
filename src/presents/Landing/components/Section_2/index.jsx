00d27da4f086e13f1c710640de80286c5fa1039f
Pedro Augusto <p.augusto0210@gmail.com>
2020-12-17 19:14:07 -0200

    ADD: Second section to Landing

---
+++ src/presents/Landing/components/Section_2/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import './styles.css'
+
+const Section2 = () => {
+  return (
+    <div id="section-2">
+      <div className="section-2-content">
+        <div className="section-2-item first-item">
+          <span className="section-2-path_line"></span>
+          <span className="section-2-item_label_number">1</span>
+          <div className="section-2-item-content">
+            <h4 className="section-2-item-content-title">
+              <span>UX/UI</span>
+            </h4>
+
+            <p className="section-2-item-content-description">Nossos desenvolvedores são apaixonados por explorar novas ferramentas de desenvolvimento, a fim de melhorar a experiência dos clientes finais.</p>
+          </div>
+        </div>
+
+        <div className="section-2-item second-item">
+          <span className="section-2-path_line"></span>
+          <span className="section-2-item_label_number">2</span>
+          <div className="section-2-item-content">
+            <h4 className="section-2-item-content-title">
+              <span>Desenvolvimento</span>
+            </h4>
+
+            <p className="section-2-item-content-description">Nossos desenvolvedores são apaixonados por explorar novas ferramentas de desenvolvimento, a fim de melhorar a experiência dos clientes finais.</p>
+          </div>
+        </div>
+
+        <div className="section-2-item third-item">
+          <span className="section-2-path_line"></span>
+          <span className="section-2-item_label_number">3</span>
+          <div className="section-2-item-content">
+            <h4 className="section-2-item-content-title">
+              <span>Produção</span>
+            </h4>
+
+            <p className="section-2-item-content-description">Nossos desenvolvedores são apaixonados por explorar novas ferramentas de desenvolvimento, a fim de melhorar a experiência dos clientes finais.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Section2
--- src/presents/Landing/index.jsx
+++ src/presents/Landing/index.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import './styles.css'
 
 import Section1 from './components/Section_1/index'
+import Section2 from './components/Section_2/index'
 
 const Landing = () => {
   return (
     <div id="landing-page">
       <div className="landing-content">
         <Section1 />
+        
+        <Section2 />
       </div>
     </div>
   )
