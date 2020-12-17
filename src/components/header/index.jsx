5aef923dfb72268d71cba2b081913d4a14fd1738
FeSilva-dev <feliper.silva011@gmail.com>
2020-12-16 22:56:02 -0300

    header complete

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
 import DevLabsLogo from '../../assets/dl-logo.svg'
+import './styles.css'
 
 const Header = () => {
   return (
     <div id="header">
-      <div className="header-content">
-        <img src={DevLabsLogo} />
+      <div id="container">
+        <div className="logo-img">
+          <img src={DevLabsLogo} alt="logo image"/>
+        </div>
+
+        <div className="menu">
+          <ul>
+            <li><a className="menu-select" href="">Inicio</a></li>
+            <li><a href="">Tecnologias</a></li>
+            <li><a href="">Projetos</a></li>
+            <li><a href="">Sobre</a></li>
+          </ul>
+        </div>
+
+        <div className="donate">
+          <a href="">Contribuir</a>
+        </div>
       </div>
     </div>
   )
--- src/presents/Landing/index.jsx
+++ src/presents/Landing/index.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Landing = () => {
   return (
     <div id="landing-page">
-      <h1>Bem vindo</h1>
-      <h4>DevLabs</h4>
+
     </div>
   )
 }
