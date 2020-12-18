29800e446b676d8291cc97856d17e79b36388489
FeSilva-dev <feliper.silva011@gmail.com>
2020-12-17 23:33:06 -0300

    video added

--- src/presents/Landing/components/Section_5/index.jsx
+++ src/presents/Landing/components/Section_5/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Iframe from 'react-iframe'
 
 import './styles.css'
 
@@ -6,7 +7,13 @@ const Section5 = () => {
     return(
         <div id="section5">
             <div className="section5-content">
-                <div className="video-about"></div>
+                <div className="video-about">
+                    <Iframe 
+                    width="100%" 
+                    height="100%" 
+                    src="https://www.youtube.com/embed/bGsZc7NoO5o"
+                    allowfullscreen frameBorder="0" />
+                </div>
                 <div className="text-about">
                     <div className="title-main">
                         <h2>Por que contribuir <br/> com a gente?</h2>
